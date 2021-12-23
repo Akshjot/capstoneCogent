4b729574ef8affded737066bb91d508e4f10f772
Akshjot Sangha <91076463+Akshjot@users.noreply.github.com>
2021-12-23 09:25:10 -0500

    Delete examserver directory

--- examserver/src/main/java/com/nt/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nt.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.nt.model.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-
-}
